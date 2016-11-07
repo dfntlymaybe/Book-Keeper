421207e6202d83b6b6b70d640038eff6b4853cee
Maor <maorfeldinger@gmail.com>
2016-11-07 10:52:40 +0200

    create button excercise

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 console.log(document.getElementById('t1').innerHTML);
 document.getElementById('t1').onclick = function(){
-  debugger;
     if (document.getElementById('t1').innerHTML === "Welcome to our Book Store"){
       document.getElementById('t1').innerHTML = "Read More Books";
     }else{
       document.getElementById('t1').innerHTML = "Welcome to our Book Store";
     }
 }
+
+var createButton = function(){
+  var btn = document.createElement("button");
+  btn.appendChild(document.createTextNode("Click"));
+  btn.onclick = createButton;
+  document.getElementById('innerdiv').appendChild(btn);
+}
+var btnClicked = document.getElementsByClassName('btn')[0];
+btnClicked.onclick = createButton;
